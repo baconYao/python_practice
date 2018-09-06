bfe81eef8267592aff7fda56d734c1099d581ae9
QNAP_MAC <qnap_mac@QNAP-MACde-MacBook-Pro.local>
2018-09-06 14:55:34 +0800

    Add some functions and test units

--- ch4_Unit_Test/test_todo.py
+++ ch4_Unit_Test/test_todo.py
@@ -21,7 +21,7 @@ def test_get_function():
 
 def test_get_fields():
     assert (todo.get_fields('new') == ['title', 'description', 'level'])
-    print "OK - test_get_fields"
+    print "OK - get_fields"
 
 def test_run_command():
     # Create "test" command
@@ -41,7 +41,7 @@ def test_show_todos():
     todo.todos = [
         {
             'title': 'test todo',
-            'description': 'This is test',
+            'description': 'This is a test',
             'level': 'Important'
         }
     ]
@@ -60,13 +60,75 @@ def test_show_todos():
     assert "1" in second_line
     assert "test todo" in second_line
     assert "This is a test" in second_line
-    assert "Important" in second_line
+    assert "IMPORTANT" in second_line
 
     print "OK - show_todos"
+
+def test_todo_sort_order():
+    todo.todos = [
+        {
+            'title': 'test unimportant todo',
+            'description': 'An unimportant test',
+            'level': 'Unimportant'
+        },
+        {
+            'title': 'test medium todo',
+            'description': 'A test',
+            'level': 'Medium'
+        },
+        {
+            'title': 'test important todo',
+            'description': 'This is an important test',
+            'level': 'Important'
+        }
+    ]
+    result = todo.show_todos(todo.todos)
+    lines = result.split("\n")
+
+    assert "IMPORTANT" in lines[1]
+    assert "Medium" in lines[3]
+    assert "Unimportant" in lines[4]
+
+    print "OK - todo sort order"
+
+def test_todo_wrap_long_lines():
+    todo.todos = [
+        {
+            'title': 'test important todo',
+            'description': (
+                'This is an important '
+                'test. We\'d really like '
+                'this line to wrap '
+                'several times, to '
+                'imitate what might '
+                'happen in a real '
+                'program.'
+            ),
+            'level': 'Important'
+        }
+    ]
+    result = todo.show_todos(todo.todos)
+    lines = result.split("\n")
+
+    assert 'test important' in lines[1]
+    assert 'This is an important' in lines[1]
+
+    assert 'todo' in lines[2]
+    assert 'test. We\'d really like' in lines[2]
+
+    assert 'this line to wrap' in lines[3]
+    assert 'several times, to' in lines[4]
+    assert 'imitate what might' in lines[5]
+    assert 'happen in a real' in lines[6]
+    assert 'program.' in lines[7]
+
+    print "OK - todo wrap long lines"
 
 # Run test
 test_create_todo()
 test_get_function()
 test_get_fields()
 test_run_command()
-
+test_show_todos()
+test_todo_sort_order()
+test_todo_wrap_long_lines()
--- ch4_Unit_Test/todo.py
+++ ch4_Unit_Test/todo.py
@@ -1,3 +1,7 @@
+import textwrap
+
+todos = []
+
 def create_todo(todos, title, description, level):
     """Create an todo item"""
     todo = {
@@ -10,13 +14,6 @@ def create_todo(todos, title, description, level):
 def test(todos, abcd, ijkl):
     return "Command 'test' returned:\n" + \
         "abcd: " + abcd + "\nijkl: " + ijkl
-
-# Given the name of the command you want to run, get_function will return the function you need to call
-commands = {
-    # Key: command, value: the function that will be called
-    'new': [create_todo, ['title', 'description', 'level']],
-    'test': [test, ['abcd', 'ijkl']]
-}
 
 # Get command's first value, means that the function will be called
 def get_function(command_name):
@@ -31,7 +28,6 @@ def get_input(fields):
         user_input[field] = raw_input(field + " > ")
     return user_input
 
-todos = []
 
 def run_command(user_input, data=None):
     user_input = user_input.lower()
@@ -48,6 +44,62 @@ def run_command(user_input, data=None):
         data = get_input(the_fields)
     # Call function
     return the_func(todos, **data)
+
+def capitalize(todo):
+    todo['level'] = todo['level'].upper()
+    return todo
+
+def show_todo(todo, index):
+    # wrapping title and description
+    wrapped_title = textwrap.wrap(todo['title'], 16)
+    wrapped_descr = textwrap.wrap(todo['description'], 24)
+    # output first line
+    output = str(index+1).ljust(8) + " "
+    output += wrapped_title[0].ljust(16) + " "
+    output += wrapped_descr[0].ljust(24) + " "
+    output += todo['level'].ljust(16)
+    output += "\n"
+
+    max_len = max(len(wrapped_title), len(wrapped_descr))
+    # output any remaining lines
+    for index in range(1, max_len):
+        output += " " *8 + " "
+        if index < len(wrapped_title):
+            output += wrapped_title[index].ljust(16) + " "
+        else:
+            output += " " *16 + " "
+        if index < len(wrapped_descr):
+            output += wrapped_descr[index].ljust(24) + " "
+        else:
+            output += " " *24 + " "
+        output += "\n"
+    return output
+
+def sort_todos(todos):
+    # Use list comprehension style to filter to-dos
+    important = [capitalize(todo) for todo in todos if todo['level'].lower() == 'important']
+    unimportant = [todo for todo in todos if todo['level'].lower() == 'unimportant']
+    medium = [todo for todo in todos if todo['level'].lower() != 'important' and todo['level'].lower() != 'unimportant']
+    # Join to-dos back up
+    sorted_todos = important + medium + unimportant
+    return sorted_todos
+
+def show_todos(todos):
+    output = ("Item     Title            Description              Level\n")
+    sorted_todos = sort_todos(todos)
+    for index, todo in enumerate(sorted_todos):
+        output += show_todo(todo, index)
+    # print output
+    return output
+
+# Given the name of the command you want to run, get_function will return the function you need to call
+commands = {
+    # Key: command, value: the function that will be called
+    'new': [create_todo, ['title', 'description', 'level']],
+    'test': [test, ['abcd', 'ijkl']],
+    'show': [show_todos, []]
+}
+
 
 def main_loop():
     user_input = ""
